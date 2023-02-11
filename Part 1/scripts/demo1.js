79813bd2a57d3d9e2f9f0bcccae1d700e9df7b90
krajeet35 <krajeet35@gmail.com>
2023-02-12 01:03:04 +0530

    1

---
+++ Part 1/scripts/demo1.js
@@ -0,0 +1,17 @@
+console.log("Ajeet Roy");
+let x=10, y=5;
+console.log(x+y);
+const pi=3.14;
+// pi=5;
+let name="   Ajeet   ";
+for(let i=0;i<name.length;i++){
+    console.log(name[i]);
+}
+name=name.trim();
+for(let i=0;i<name.length;i++){
+    console.log(name[i]);
+}
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+name=name.slice(1,3)
+console.log(name);
