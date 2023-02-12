40cf5f0a6a2422315df91fff14877e9ee8a974f4
krajeet35 <krajeet35@gmail.com>
2023-02-12 21:21:49 +0530

    4

--- Part 1/scripts/objects.js
+++ Part 1/scripts/objects.js
@@ -67,10 +67,151 @@
 
 //  Functions
 
-function print(){
-    console.log("Hello There!");
-}
-print();print();print();
+// //  Function declaration
+// function print(){
+//     console.log("Hello There!");
+// }
+// print();
+
+// //  function expression
+// let sum= function(a,b){
+//     return a+b;
+// }
+
+// // Arrow function
+// let divide= (a,b)=>{
+//     return a/b;
+// }
+
+// let mult = (a,b) => a*b;
+// console.log(mult(4,5));
+
+// //  function inside function
+
+// let fun1 = ()=>{
+//     console.log("inside fun1");
+    
+//     let fun2 = ()=>{
+//         console.log("inside fun2");
+//     }
+//     fun2();
+// }
+// fun1();
+
+// //  Rest parameter
+
+// let fun3= (a,b,...c) =>{
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+// fun3(5,4,2,3,7,6,9)
+
+// let fun4= (...a) =>{
+//     let sum=0;
+//     for(let i of a){
+//         sum+=i;
+//     }
+//     console.log(sum);
+// }
+// fun4(5,4,2,3,7,6,9)
+
+// parameter destructuring
+
+// let person={
+//     name: "Ajeet",
+//     last: "Roy"
+// };
+
+// let details= (obj) =>{
+//     console.log(obj.name);
+//     console.log(obj.last);
+// }
+// let details= ({last,name}) =>{
+//     console.log(name);
+//     console.log(last);
+// }
+
+// details(person);
+
+// callback function
+
+// let fun1 = (a)=>{
+//     a();
+
+// }
+
+// let fun2 =()=>{
+//     console.log("inside fun2");
+// }
+// fun1(fun2);
+
+//  function returning function
+
+
+
+// let fun1= ()=>{
+//     let fun2 = ()=>{
+//         return "inside fun2 "
+//     }
+//     return fun2;
+// }
+
+// let res=fun1();
+// console.log(res());
+
+//  forEach
+// let arr=[5,4,2,13,6,7,5,8,7];
+// arr.forEach((i,j)=>{
+//     console.log(i,j);
+// });
+// let newarr=arr.map((i)=>{
+//     return (i*10);
+// });
+// console.log(newarr);
+
+// let newarr= arr.filter((i)=>{
+//     return i%2==0;
+// });
+// console.log(newarr);
+
+
+// let val=arr.reduce((a,b,i,j)=>{
+//     console.log(a,b,i,j);
+//  return a+b;
+// });
+
+// console.log(val);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
