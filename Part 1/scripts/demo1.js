d84b531cdc24000e1176e6ebed80b933b35ddf4b
krajeet35 <krajeet35@gmail.com>
2023-02-12 14:09:23 +0530

    2

--- Part 1/scripts/demo1.js
+++ Part 1/scripts/demo1.js
@@ -1,17 +1,86 @@
-console.log("Ajeet Roy");
-let x=10, y=5;
-console.log(x+y);
-const pi=3.14;
-// pi=5;
-let name="   Ajeet   ";
-for(let i=0;i<name.length;i++){
-    console.log(name[i]);
-}
-name=name.trim();
-for(let i=0;i<name.length;i++){
-    console.log(name[i]);
-}
-console.log(name.toUpperCase());
-console.log(name.toLowerCase());
-name=name.slice(1,3)
-console.log(name);
+// console.log("Ajeet Roy");
+// let x=10, y=5;
+// console.log(x+y);
+// const pi=3.14;
+// // pi=5;
+// let name="   Ajeet   ";
+// for(let i=0;i<name.length;i++){
+//     console.log(name[i]);
+// }
+// name=name.trim();
+// for(let i=0;i<name.length;i++){
+//     console.log(name[i]);
+// }
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// name=name.slice(1,3)
+// console.log(name);
+// console.log("1"==1);
+// console.log("1"===1);
+
+// let age=20;
+
+
+// let voter=age>=18 ? vote="yes" : vote="no";
+// console.log(voter);
+
+// // Switch
+// // let day= prompt("Enter a number"); 
+// let day=4;
+
+// switch(day){
+//     case 0: console.log("Sunday");
+//     break;
+//     case 1: console.log("Monday");
+//     break;
+//     case 2: console.log("Tuesday");
+//     break;
+//     case 3: console.log("Wednesday");
+//     break;
+//     case 4: console.log("Thursday");
+//     break;
+//     case 5: console.log("Friday");
+//     break;
+//     case 6: console.log("Saturday");
+//     break;
+//     default: console.log("Invalid Input");
+//     break;
+// }
+// let i=0;
+// while(i<5){
+//     console.log(i);
+//     i++;
+// }
+// let j=0;
+// do{
+//     console.log(j);
+//     j++;
+    
+// }while(j<5);
+// console.log("New");
+
+let arr=[5,3,6,8,4,12,8];
+arr.push(9);
+arr.pop();
+arr.shift();
+arr.unshift(1);
+console.log(arr);
+let newarr=[].concat(arr);
+console.log(newarr);
+// Spread operator
+let arr1=[...arr,"j","roy"];
+console.log(arr1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
